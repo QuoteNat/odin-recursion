cab7fe02cae91e2065d3f5f47ca7310505794639
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2025-04-17 13:29:53 -0500

    Test a sequence of numbers with both fib functions

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 import { fibs, fibsRec } from "./fibs.js";
 
-const testFibs = 8;
-
-console.log(
-  `fibs(${testFibs}) is [${fibs(
-    testFibs
-  )}] and fibsRec(${testFibs}) is [${fibsRec(8)}]`
-);
+for (let i = 1; i <= 10; i++) {
+  console.log(
+    `fibs(${i}) is [${fibs(i)}] and fibsRec(${i}) is [${fibsRec(i)}]`
+  );
+}
